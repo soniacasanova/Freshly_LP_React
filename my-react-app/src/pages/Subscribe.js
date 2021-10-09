04d12420dab1a346aa97b05f4f7f9f8cf7bf35cb
soniacasanova <soniacasanovar@gmail.com>
2021-10-09 16:10:59 +0200

    Subsribe submit

--- my-react-app/src/pages/Subscribe.js
+++ my-react-app/src/pages/Subscribe.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Submit from "./components/Submit";
 import './Subscribe.css'
 
 export default function Subscribe() {
@@ -10,34 +11,31 @@ export default function Subscribe() {
                    AND WIN A SPA DAY</h2>
             </div>
             <div className="row">
-                <div className="col-lg-4 col-md-4 col-xs-4 p-0 center">
+                <div className="col-lg-4 col-md-4 col-xs-4 p-1 center">
                     <div className="">
-                      <img className="image-grid img-fluid" src="" alt="1" />
-                      <img className="image-grid img-fluid" src="" alt="2" />
+                      <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1506634064465-7dab4de896ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" alt="1" />
+                      <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1620733719521-fa0625956f5c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" alt="2" />
                     </div>
                   </div>
-                  <div className="col-lg-4 col-md-4 col-xs-4 p-0 center">
+                  <div className="col-lg-4 col-md-4 col-xs-4 p-1 center">
                     <div className="">
-                        <img className="image-grid img-fluid" src="" alt="3" />
-                        <img className="image-grid img-fluid" src="" alt="4" />
+                        <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1503149779833-1de50ebe5f8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="3" />
+                        <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1568114813528-471a736a8fd3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" alt="4" />
                     </div>
                   </div>
-                  <div className="col-lg-4 col-md-4 col-xs-4 p-0 center">
+                  <div className="col-lg-4 col-md-4 col-xs-4 p-1 center">
                     <div className="">
-                        <img className="image-grid img-fluid" src="" alt="5" />
-                        <img className="image-grid img-fluid" src="" alt="6" />
+                        <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1517487881594-2787fef5ebf7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80" alt="5" />
+                        <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1525923838299-2312b60f6d69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80" alt="6" />
                     </div>
                   </div>
             </div>
             <div className="row g-3 align-items-center">
               
                 <div className="col-auto">
-                  <input type="email" id="inputEmail" className="form-control" aria-describedby="email"/>
-                </div>
-                <div className="col-auto">
-                  <button type="submit">Submit</button>
-                </div>
+                 <Submit />
               </div>
+        </div>
         </div>
         </>
     )
---
+++ my-react-app/src/pages/components/Submit.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import ReactDOM from 'react-dom';
+import './Submit.css'
+
+function Submit() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('The email you entered was: ${email}')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your email: 
+        <input 
+          type="text" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <input className="btn btn-green" type="submit" />
+    </form>
+  )
+}
+
+ReactDOM.render(<Submit />, document.getElementById('root'));
+export default Submit;
