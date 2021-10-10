e28ec57be6a798117a5285045766a568d239a7f9
soniacasanova <soniacasanovar@gmail.com>
2021-10-10 16:18:29 +0200

    footer

--- my-react-app/src/pages/Subscribe.js
+++ my-react-app/src/pages/Subscribe.js
@@ -14,13 +14,13 @@ export default function Subscribe() {
             <div className="row">
                 <div className="col-lg-4 col-md-4 col-xs-4 p-1 center">
                     <div className="">
-                      <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1506634064465-7dab4de896ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" alt="1" />
-                      <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1620733719521-fa0625956f5c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" alt="2" />
+                      <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1620733719521-fa0625956f5c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" alt="1" />
+                      <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1503149779833-1de50ebe5f8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="2" />
                     </div>
                   </div>
                   <div className="col-lg-4 col-md-4 col-xs-4 p-1 center">
                     <div className="">
-                        <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1503149779833-1de50ebe5f8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="3" />
+                        <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1506634064465-7dab4de896ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" alt="3" />
                         <img className="image-grid img-fluid p-3" src="https://images.unsplash.com/photo-1568114813528-471a736a8fd3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" alt="4" />
                     </div>
                   </div>
@@ -35,7 +35,7 @@ export default function Subscribe() {
             
             <div className="row g-3 align-items-center">
               <h2> SUBSCRIBE HERE TO OUR NEWSLETTER </h2>
-                <div className="col-auto">
+                <div className="col-auto form-email">
                  <Submit />
               </div>
         </div>
